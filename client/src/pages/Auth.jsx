dbc3f6aea9db76d31251bd8eaf14d408e35668e2
annaletyflor <annaletyflor@gmail.com>
2023-10-19 17:07:58 -0300

    Inicio feito

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,25 @@
+const Auth = () => {
+    const onSubmit = ()=>{
+
+    }
+  return (
+    <div className="background">
+      <form onSubmit={onSubmit} className="form-card">
+        <div className="form-title">Bem-vindo! 😉</div>
+        <div className="form-subtitle">
+          Informe um nome de usuário para continuar
+        </div>
+
+        <div className="auth">
+          <div className="auth-label">Username</div>
+          <input className="auth-input" name="username" />
+          <button className="auth-button" type="submit">
+            Entrar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Auth;
